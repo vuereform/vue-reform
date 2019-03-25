491e73732c6bab7bc01179f576796b1d262c186d
Les Harris <les@lesharris.com>
2019-03-24 21:21:17 -0700

    Reset :sad:

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from '@storybook/vue'
-
-const req = require.context('../stories', true, /.stories.(j|t)s$/)
-function loadStories() {
-  req.keys().forEach(filename => req(filename))
-}
-
-configure(loadStories, module)
--- tests/unit/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import VueReform from '../../src/components/VueReform/VueReform';
-
-describe('VueReform.tsx', () => {
-  it('is a vue component', () => {
-    const wrapper = shallowMount(VueReform, {})
-    expect(wrapper.isVueInstance()).toBe(true)
-  })
-})
