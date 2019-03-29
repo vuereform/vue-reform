ae6e0fa8fd5ea1ff512c9356c070997b64fa04d7
Les Harris <les@lesharris.com>
2019-03-28 18:11:45 -0700

    Remove example test, fix the build

--- tests/unit/VueReform.spec.tsx
+++ tests/unit/VueReform.spec.tsx
@@ -1,9 +1,19 @@
 import { shallowMount } from '@vue/test-utils'
 import VueReform from '@/components/VueReform'
+import DefaultRenderer from '@/components/renderers/DefaultRenderer'
 
 describe('VueReform.vue', () => {
   it('is a vue component', () => {
-    const wrapper = shallowMount(VueReform)
+    const wrapper = shallowMount(VueReform, {
+      propsData: {
+        renderer: new DefaultRenderer(),
+        formData: {
+          name: 'test',
+          label: 'test',
+          children: []
+        }
+      }
+    })
     expect(wrapper.isVueInstance()).toBe(true)
   })
 })
